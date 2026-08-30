8034e1abdd16dd542d2e7fd659eef7b98025e714
Luca Mantello <3004085@stud.hs-mannheim.de>
2026-08-30 13:45:44 +0200

    elloe

--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
 
+
 int main() {
 
-    return 0;
+/*
+    char board[8][8] = {
+
+        {'R','N','B','Q','K','B','N','R'},
+        {'P','P','P','P','P','P','P','P'},
+        {'.','.','.','.','.','.','.','.'},
+        {'.','.','.','.','.','.','.','.'},
+        {'.','.','.','.','.','.','.','.'},
+        {'.','.','.','.','.','.','.','.'},
+        {'p','p','p','p','p','p','p','p'},
+        {'r','n','b','q','k','b','n','r'}
+        };
+
+       for(int row = 0 ; row < 8; row++) {
+        for (int column = 0; column < 8; column++) {
+
+            std::cout << board[row][column] << ' ';
+        }
+        std::cout << '\n';
+
+       }
+
+*/
+       char file[8] = {'a','b','c','d','e','f','g','h'};
+
+       char row[8] = {'1','2','3','4','5','6','7','8'};
+
+
+       for(int i = 0; i< 8; i++){
+
+
+       std::cout << file[i] + row[i] << '\n';
+
+       int pos = file[i] + row[i];
+
+       int pos2
+
+    };
+        return 0;
+
+
+
+
 
 }
